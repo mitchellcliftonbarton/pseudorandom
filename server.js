c43b148d36d29894057af71bfe68dcd1e0db143f
Mitchell Barton <mitchell@ringseven.com>
2015-10-15 11:36:22 -0600

    added body parser

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var app = express();
 var path = require('path');
 var fs = require('fs');
 var winston = require('winston');
-// var parser = require('body-parser');
+var parser = require('body-parser');
 
 winston.add(winston.transports.File, { filename: 'winston.log' });
 winston.info('Hello again distributed logs');
 
 app.use(express.static('public'));
-// app.use(express.bodyParser({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
