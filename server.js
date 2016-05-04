556bdbaaef0408f7e94763a5ec74ef1ef7f90f0d
Mitchell Barton <mitchell@ringseven.com>
2016-05-03 20:59:57 -0600

    stuff

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/sand', function(req, res, next) {
 app.post('/shapes', function(req, res, next) {
   // getDurl('public/shapes/line.png');
   res.set('content-type', 'image/png');
-  res.sendFile('/shapes/line.jpg', { root: path.join(__dirname, 'public') });
+  res.sendFile('/shapes/line.png', { root: path.join(__dirname, 'public') });
 });
 
 var server = app.listen(3000, function () {
