2c25e9d4ff98933f1ff0d931ff934710cefa718b
Mitchell Barton <mitchell@ringseven.com>
2016-05-03 21:36:53 -0600

    stuff

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/sand', function(req, res, next) {
 app.post('/shapes', function(req, res, next) {
   // res.set('content-type', 'image/png');
   var img;
-  fs.readFile(path.join(__dirname, 'public') + '/shapes/line.png', function(err, data) {
+  fs.readFile('/shapes/line.png', function(err, data) {
     var base = new Buffer(data).toString('base64');
     img = base;
     
